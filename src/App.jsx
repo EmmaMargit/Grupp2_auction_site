729da653c9a1f0d0e8265a7a256e8da479956c0b
Emma Brandberg <emma94brandberg@hotmail.com>
2024-04-03 16:49:33 +0200

    ändrade footer css till module css samt imort väg

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Footer from './components/boilerplats/Footer'
+import './stylesheet/Footer.module.css';
 import './stylesheet/App.css'
 
 function App() {
--- src/components/boilerplats/Footer.jsx
+++ src/components/boilerplats/Footer.jsx
@@ -1,11 +1,11 @@
-
+import React from 'react'
 
 function Footer() {
   
 
     return (
       <div>
-        hejehjehej mmnknkkk
+        ändrat
       </div>
     )
   }
