774a61bec2a3f6b67ef48d91bd12385b8b8fcb66
Nellie Gränd <nellie.grand@hotmail.com>
2024-04-04 15:48:26 +0200

    lagt till kod i FirstPage.jsx och testfärg i FirstPage.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/boilerplats/Header'
+import FirstPage from './components/FirstPage'
 // import '../stylesheet/App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header />
+      <FirstPage />
     </>
   )
 }
---
+++ src/components/FirstPage.jsx
@@ -0,0 +1,41 @@
+import React, {useRef, useState} from "react";
+import "../stylesheet/FirstPage.css"; 
+
+function FirstPage () {
+const inputField = useRef(null); //Referens till inpufält
+const [searchResult, setSearchResult] = useState([]); //State för sökresultatet
+
+const inputHandler = (event) => {
+    const value = inputField.current.value;
+}
+
+const inputBtn = (event) => {
+event.preventDefault();
+} 
+
+//Funktion för att hämta alla auktioner
+
+function getAuctions () {
+    fetch (`länk att fetcha`)
+    .then((response) => response.json())
+      .then((result) => {
+        setMealResult(result.meals);
+      })
+      .catch((error) => {
+        console.error("Felmeddelande", error); 
+      });
+  
+}
+
+return (
+    <div className="container">
+        <form className="inputStyling" onSubmit={inputBtn}>
+        <input type="text" ref={inputField} onChange={inputHandler}/>
+        <button type="submit">Sök</button>
+        </form>
+        <h2>Alla auktioner</h2>
+    </div>
+)
+};
+
+export default FirstPage;
