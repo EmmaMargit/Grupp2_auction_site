5d34bd5db310875b82dd02371df3e2838f06d10c
Nellie Gränd <nellie.grand@hotmail.com>
2024-04-06 13:25:25 +0200

    testat kommentera bort lite kod pga error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/boilerplats/Header'
 import FirstPage from './components/boilerplats/FirstPage'
-// import '../stylesheet/App.css'
+/* import './stylesheet/App.css' */
 
 function App() {
 
