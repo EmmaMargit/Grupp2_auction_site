b0b83acc4898764d64e6a28d27fb4d34492f1c88
Emma Brandberg <emma94brandberg@hotmail.com>
2024-04-08 17:10:29 +0200

    kommenterade bort saker som var felmeddlande på

--- src/components/boilerplats/Details.jsx
+++ src/components/boilerplats/Details.jsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-function Details() {
-  const [getAuctionById, setGetAuctionById] = useState([]);
+// function Details() {
+//   const [getAuctionById, setGetAuctionById] = useState([]);
 
-  useEffect(() => {
-    const getSpecifikAuction = () => {
-      fetch(`https://auctioneer2.azurewebsites.net/auction/2wvu/${AuctionID}`)
-        .then((res) => res.json())
-        .then((data) => setGetAuctionById(data));
-    };
-    getSpecifikAuction();
-  }, []);
-  return (
-    <div>
-      <h1>Testar data</h1>
-      <ul>
-        {getAuctionById &&
-          Array.isArray(getAuctionById) &&
-          getAuctionById.map((item, index) => (
-            <li key={index}>
-              {item.Title} {item.Description}
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-}
+//   useEffect(() => {
+//     const getSpecifikAuction = () => {
+//       fetch(`https://auctioneer2.azurewebsites.net/auction/2wvu/${AuctionID}`)
+//         .then((res) => res.json())
+//         .then((data) => setGetAuctionById(data));
+//     };
+//     getSpecifikAuction();
+//   }, []);
+//   return (
+//     <div>
+//       <h1>Testar data</h1>
+//       <ul>
+//         {getAuctionById &&
+//           Array.isArray(getAuctionById) &&
+//           getAuctionById.map((item, index) => (
+//             <li key={index}>
+//               {item.Title} {item.Description}
+//             </li>
+//           ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default Details;
+// export default Details;
