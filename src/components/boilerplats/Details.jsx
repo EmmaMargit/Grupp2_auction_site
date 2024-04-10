56fa216fe5582b724ecd2e2ca48f5cbba95f3f18
Robin Bal <balrobin@hotmail.com>
2024-04-11 01:35:33 +0200

    Lagt in info i details komponent

--- src/components/boilerplats/Details.jsx
+++ src/components/boilerplats/Details.jsx
@@ -4,12 +4,22 @@ import { useLocation } from "react-router-dom";
 function Details() {
   const location = useLocation();
   const { auction } = location.state;
-  console.log(auction);
+
+  const formatDate = (dateString) => {
+    const postDate = new Date(dateString);
+    const formattedDate = `${postDate.getFullYear()}-${
+      postDate.getMonth() + 1
+    }-${postDate.getDate()} ${postDate.toLocaleTimeString()}`;
+    return formattedDate;
+  };
 
   return (
     <div style={{ marginTop: "40vh" }}>
       <h3>{auction.Title}</h3>
-      <small>Robin hej hej</small>
+      <p>Description: {auction.Description}</p>
+      <p>Start Date: {formatDate(auction.StartDate)}</p>
+      <p>End Date: {formatDate(auction.EndDate)}</p>
+      <p>Starting Price: {auction.StartingPrice}</p>
     </div>
   );
 }
