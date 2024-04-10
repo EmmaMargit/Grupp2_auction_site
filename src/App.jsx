0784293978537a066bd0b59c2ee1c6dd844e064f
Emma Brandberg <emma94brandberg@hotmail.com>
2024-04-10 11:36:50 +0200

    mellanrum

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Footer from "./components/boilerplats/Footer";
 import "./stylesheet/Footer.module.css";
 import "./stylesheet/App.css";
-
 // import Details from "./components/boilerplats/Details";
-
 import Header from './components/boilerplats/Header'
 import FirstPage from './components/boilerplats/FirstPage'
 
