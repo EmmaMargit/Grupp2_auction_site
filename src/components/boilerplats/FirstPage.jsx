63c52ee692b40cd121b8f921d2c314a6802ba977
Nellie Gränd <nellie.grand@hotmail.com>
2024-04-15 16:18:52 +0200

    lagt in funktionalitet för att söka auktioner

--- src/components/boilerplats/FirstPage.jsx
+++ src/components/boilerplats/FirstPage.jsx
@@ -1,27 +1,30 @@
-import React, {useRef, useState, useEffect } from "react";
-import "../../stylesheet/FirstPage.css"
+import React, { useRef, useState, useEffect } from "react";
+import "../../stylesheet/FirstPage.css";
 import { Link } from "react-router-dom";
 
 function FirstPage() {
   // Alla auktioner
   const [auctions, setAuctions] = useState([]);
-  const inputField = useRef(null); //hänvisa till input
-  const [searchItem, setSearchItem] = useState("");
-  //const [searchResult, setSearchResult] = useState([]); //state för sökresultatet
+  const [allAuctions, setAllAuctions] = useState([]); // En state för att spara alla auktioner
 
+  //snappa upp sökfältet
+  const inputField = useRef(null); //hänvisa till input
+
+  // Hämta alla auktioner på startsidan på en gång
   useEffect(() => {
     getAuctions();
   }, []);
 
-  const handleInputField = (event) => {
-    const searchItem = inputField.current.value;
-  };
-
+  // Eventhandler när man sökt på specifik auktion eller ej
   const handleInputBtn = (event) => {
     event.preventDefault();
-    getSearchedAuctions(searchItem);
-  }
-
+    const searchItem = inputField.current.value.trim();
+    if (searchItem === "") {
+      getAuctions();
+    } else {
+      getSearchedAuctions(searchItem);
+    }
+  };
 
   // Funktion för att hämta alla auktioner
   function getAuctions() {
@@ -31,39 +34,42 @@ function FirstPage() {
         // Kolla om det finns auktioner
         if (result && result.length > 0) {
           setAuctions(result);
+          setAllAuctions(result); // Spara alla auktioner i en separat state
         }
       })
       .catch((error) => {
-        console.error("Felmeddelande", error);
+        console.error("Felmeddelande", error); // Felmeddalande om det ej finns auktioner
       });
   }
 
-  //Funktion för att hämta specifik auktion efter sökning
-function getSearchedAuctions(searchItem) {
-  fetch(`https://auctioneer2.azurewebsites.net/auction/2wvu/${searchItem}`)
-    .then((response) => response.json())
-    .then((result) => {
-      // Uppdatera listan med alla auktioner med sökresultatet
-      setAuctions(result.auctions);
-    })
-    .catch((error) => {
-      console.error("Felmeddelande", error);
-    });
-}
+  // Funktion för att hämta specifik auktion efter sökning
+  function getSearchedAuctions(searchItem) {
+    const specificAuctions = allAuctions.filter((auction) =>
+      auction.Title.toLowerCase().includes(searchItem.toLowerCase())
+    );
+    setAuctions(specificAuctions); // Uppdatera state med de sökta auktionsorden
+  }
 
   return (
     <>
       <div className="container">
-
-      <form onSubmit={handleInputBtn}>
-        <input type="text" id="searchBar" ref={inputField} onChange={handleInputField} placeholder="Search" />
-        <button type="submit" id="searchBtn">Sök auktion</button>
+        <form onSubmit={handleInputBtn}>
+          <input
+            type="text"
+            id="searchBar"
+            ref={inputField}
+            placeholder="Search"
+          />
+          <button type="submit" id="searchBtn">
+            Sök auktion
+          </button>
         </form>
         <h2>Alla auktioner</h2>
         {auctions && auctions.length > 0 && (
           <ul>
             {auctions.map((auction, index) => (
-              <ul key={index} > 
+              <ul key={index}>
+                {/* Vi valde att endast ha med nedan info på startsidan, detaljvyn visar mer info */}
                 <h2 id="auctionTitle">{auction.Title}</h2>
                 <h3 id="auctionStartingPrice">{auction.StartingPrice}</h3>
                 <h3 id="auctionEndDate">{auction.EndDate}</h3>
@@ -78,13 +84,10 @@ function getSearchedAuctions(searchItem) {
           <h4>Konst (3)</h4>
           <h4>Klockor (6)</h4>
           <h4>Böcker (4)</h4>
-
         </div>
-
       </div>
     </>
   );
 }
 
-export default FirstPage; 
-
+export default FirstPage;
