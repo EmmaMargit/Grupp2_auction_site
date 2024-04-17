58593c9fede22fce3285221bc9b484e46d34db38
Marvin Barrett <marvin.barrett@mb@gmail.com>
2024-04-17 12:13:46 +0200

    fixat route till annonsformuläret

--- src/components/boilerplats/Header.jsx
+++ src/components/boilerplats/Header.jsx
@@ -3,7 +3,7 @@ import "@fontsource/pacifico"
 import "@fontsource/pacifico/400.css"
 import "@fontsource/pacifico/cyrillic-400.css"
 import '../../stylesheet/Header.css';
-
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
@@ -12,7 +12,7 @@ const Header = () => {
             <div className="logo">Bidmaster</div>
             <nav>
                 <ul className="nav-links">
-                    <li><a href="#">Lägg till annons</a></li>
+                    <li><Link to="/create-auction">Skapa annons</Link> </li>
                     <li><a href="#">Skapa konto</a></li>
                     <li><a href="#">Logga in</a></li>
                     <li><a href="#">English</a></li>
@@ -21,19 +21,6 @@ const Header = () => {
         </header>
     )
 };
-
-
-/*const Header = () => {
-
-    return (
-        <>
-            <header style={{ backgroundColor: "#1E253C", minHeight: "273px" }}>
-                <p style={{ color: "#FFFFFF", fontFamily: "Pacifico", fontSize: "48px", marginTop: "0", paddingTop: "40px", paddingLeft: "40px" }}>Bidmaster</p>
-            </header>
-        </>
-    )
-}
-*/
 
 
 export default Header
