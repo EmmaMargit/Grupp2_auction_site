f06682c7d5008273df09e658eaf331e361b732e1
Robin Bal <balrobin@hotmail.com>
2024-04-19 01:22:27 +0200

    Made the "logo" of the header to a link back to firstpage.

--- src/components/boilerplats/Header.jsx
+++ src/components/boilerplats/Header.jsx
@@ -1,26 +1,33 @@
-
-import "@fontsource/pacifico"
-import "@fontsource/pacifico/400.css"
-import "@fontsource/pacifico/cyrillic-400.css"
-import '../../stylesheet/Header.css';
+import "@fontsource/pacifico";
+import "@fontsource/pacifico/400.css";
+import "@fontsource/pacifico/cyrillic-400.css";
+import "../../stylesheet/Header.css";
 import { Link } from "react-router-dom";
 
-
 const Header = () => {
-    return (
-        <header>
-            <div className="logo">Bidmaster</div>
-            <nav>
-                <ul className="nav-links">
-                    <li><Link to="/create-auction">Skapa annons</Link> </li>
-                    <li><a href="#">Skapa konto</a></li>
-                    <li><a href="#">Logga in</a></li>
-                    <li><a href="#">English</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+        <div className="logo">Bidmaster</div>
+      </Link>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/create-auction">Skapa annons</Link>{" "}
+          </li>
+          <li>
+            <a href="#">Skapa konto</a>
+          </li>
+          <li>
+            <a href="#">Logga in</a>
+          </li>
+          <li>
+            <a href="#">English</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
-
-export default Header
+export default Header;
